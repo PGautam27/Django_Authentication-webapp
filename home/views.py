fbf94e006d54270e1626539c75979cd93b1070ed
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-03 15:48:16 +0530

    Second Django project

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def index(request):
+    render(request, "index.html")
+
+
+def login(request):
+    render(request, "login.html")
+
+
+def logout(request):
+    render(request, "index.html")
