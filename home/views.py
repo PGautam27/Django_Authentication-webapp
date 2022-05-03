ae4d6aceb11d3a30af31faa0ddb87ef102c52c54
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-03 16:49:54 +0530

    Second Django project

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, logout, login
 
 
 # Create your views here.
@@ -10,17 +9,18 @@ def index(request):
     return render(request, "index.html")
 
 
-def login(request):
+def loginUser(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
+        print(username, password)
         user = authenticate(username=username, password=password)
         if user is not None:
+            login(request, user)
             return redirect("/")
-        else:
-            return render(request, "login.html")
+    return render(request, "login.html")
 
 
 def logoutUser(request):
     logout(request)
-    return render(request, "index.html")
+    return redirect("/login")
